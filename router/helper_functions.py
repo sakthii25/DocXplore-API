2686ad574191568964532b10ab903dee81edb740
sakthii25 <sakthinarayananr25@gmail.com>
2024-09-07 17:57:56 +0530

    changed to fastapi

--- router/helper_functions.py
+++ router/helper_functions.py
@@ -1,4 +1,5 @@
 from core.vectordb import QdrantDB
+from pymongo import MongoClient
 from qdrant_client.models import VectorParams,Distance
 
 def create_collections(req:dict):
@@ -20,3 +21,28 @@ def delete_collections(req:dict):
     db.delete_collection(collection_name=collection_name)
 
     return {"message" : "Successfully deleted the collection"}
+
+
+class AtlasClient ():
+
+   def __init__ (self, altas_uri, dbname):
+       self.mongodb_client = MongoClient(altas_uri)
+       self.database = self.mongodb_client[dbname]
+
+   ## A quick way to test if we can connect to Atlas instance
+   def ping (self):
+       self.mongodb_client.admin.command('ping')
+
+   def get_collection (self, collection_name):
+       collection = self.database[collection_name]
+       return collection
+   
+   def insert(self,collection_name,document):
+       collection = self.database[collection_name]
+       return collection.insert_one(document)
+
+   def find(self, collection_name, filter = {}):
+       collection = self.database[collection_name]
+       item = collection.find_one(filter=filter)
+       return item
+   
--- test.py
+++ test.py
@@ -3,20 +3,30 @@
 # obj = IndexDocs()
 
 # path = "/Users/sakthi.n/Documents/clg/DocXplore-API/test.txt"
-# path = "/Users/sakthi.n/Documents/Project/spyo/Archive0/4_hyper-checkout_android_base-sdk-integration_getting-sdk.md"
+# path = "/Users/sakthi.n/Documents/Project/spyo/Archive0/102_hyper-checkout_android_base-sdk-integration_order-status-api.md"
 # res = obj.index(path,collection_name='CLG')
 
 # print(res)
 
-from router.query.chat_docs import ChatDocs 
+# from router.query.chat_docs import ChatDocs 
 
-obj = ChatDocs() 
+# obj = ChatDocs() 
 
-res = obj.chat(query="how to get the hyperchekout SDK in java",collection_name="CLG")
-print(res)
+# res = obj.chat(query="Give me a sample request and response example for order status api",collection_name="CLG")
+# print(res)
 
 # print(res.metadata['prompt'])
 # print(res.metadata['response'])
 # To index
 # curl 'http://127.0.0.1:5215/index-docs' -X POST -H 'Content-Type: application/json' --data-raw '{"path":"/Users/sakthi.n/Documents/clg/DocXplore-API/test.txt"}'
 
+
+from router.helper_functions import AtlasClient 
+
+uri = "mongodb+srv://sakthii25:#Sakpymango25@test.ddrmsow.mongodb.net/?retryWrites=true&w=majority&appName=Test"
+
+cli = AtlasClient(uri,'Test') 
+# print(cli.get_collection('API-KEY'))
+
+# print(cli.insert('API-KEY',{'key' : 123456789}))
+print(cli.insert('API-KEY',{'key' : "123456789"}))
